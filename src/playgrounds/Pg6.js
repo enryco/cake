5f28367abfe5687d68f737351d65fe75207f0763
Enrico Scherlies <e.scherlies@me.com>
2017-12-21 12:08:28 +0100

    arrow key navigatio

--- src/playgrounds/Pg6.js
+++ src/playgrounds/Pg6.js
@@ -15,19 +15,48 @@ class Playground extends Component {
   }
 
   componentDidMount() {
+    document.onkeydown = e => {
+      switch (e.key) {
+        case 'ArrowUp':
+          this.navigate.prev()
+          break;
+        case 'ArrowDown':
+          this.navigate.next()
+          break;
+        default:
+          break;
+      }
+    }
   }
 
+  navigate = {
+    prev : () => {
+      const active = this.state.active
+      if (active > 0) {
+        this.setState({ active: active - 1 })
+      }
+
+    },
+    next : () => {
+      const active = this.state.active
+      const len = this.state.menu.length
+      if (active < len - 1) {
+        this.setState({ active: active + 1 })
+      }
+    }
+  }
 
 
   render() {
     const menu = this.state.menu
     const upperPlaceholder = _.slice(menu, this.state.active)
+    const timesPlaceholder = _.times(menu.length - this.state.active - 1)
 
     return (
       <div className="p6-dynamic-menu">
         <div className="p6-dynamic-menu__menu">
           {
-            _.times(menu.length - this.state.active - 1, (e, i) => <div key={i + 10} className="p6-dynamic-menu__item p6-dynamic-menu__placeholder">placeholder</div>)
+            _.map(timesPlaceholder, (e, i) => <div key={i + 10} className="p6-dynamic-menu__item p6-dynamic-menu__placeholder">placeholder</div>)
           }
           {
             _.map(menu, (e, i) => <div className={`p6-dynamic-menu__item ${this.state.active === i ? 'active' : ''}`} onClick={() => this.setState({ active: i })} key={i}>{e.title}</div>)
